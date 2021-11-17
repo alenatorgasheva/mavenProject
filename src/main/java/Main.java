33f7afab7a9f892c876f90fdc82bd93704190ed0
Alena <in-fa@mail.ru>
2021-11-17 20:59:08 +0700

    В Service включены действия с базой данных.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,13 +10,17 @@ public class Main {
 
         String answer = "+";
         while (answer.equals("+")) {
-            System.out.println("\nВведите дату в формате dd/MM/yyyy, например, 21/02/2019: ");
-            String data = in.nextLine();
-            System.out.println("Введите идентификатор валюты, например, USD: ");
-            String currencyName = in.nextLine();
+            //System.out.println("\nВведите дату в формате dd/MM/yyyy, например, 21/02/2019: ");
+            //String data = in.nextLine();
+            //System.out.println("Введите идентификатор валюты, например, USD: ");
+            //String currencyName = in.nextLine();
+
+            String data = "21/01/2019";
+            String currencyName = "usd";
 
             try {
                 Currency currency = new Currency(data, currencyName);
+                Service.updateDatabase(currency);
                 Service.printExchangeRate(currency);
             } catch (EmptyInputDataException e) {
                 e.printStackTrace();
@@ -24,7 +28,8 @@ public class Main {
                 e.printStackTrace();
             }
             System.out.print("\nПродолжить? (+/-): ");
-            answer = in.nextLine();
+            //answer = in.nextLine();
+            answer = "-";
         }
     }
 }
